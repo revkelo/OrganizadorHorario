ee828b0ed045e30eabe3e325b6a1648adbd0cee4
Kevin Gonzalez <105946335+revkelo@users.noreply.github.com>
2023-12-04 21:44:18 -0500

    Agregar Y mostrar Horario

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import view.FachadaView;
-import view.Ventana;
 
 public class Controller implements ActionListener {
 
@@ -12,13 +11,40 @@ public class Controller implements ActionListener {
 
 	public Controller() {
 		vp = new FachadaView();
-
+		actionsListeners();
 		vp.getView().setVisible(true);
+	}
+
+	public void actionsListeners() {
+		vp.getView().getBtnAgregar().addActionListener(this);
+		vp.getView().getExitMenuItem().addActionListener(this);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		String comando = e.getActionCommand();
+		if (comando.equals("Agregar")) {
+			try {
+
+				int hinicial = vp.getMsj().tomarInt("Hora Inicial") - 7;
+				int hfinal = vp.getMsj().tomarInt("Hora Final") -8;
+				int diaSemana = vp.getMsj().tomarInt("Dia que ves la asignatura ejemplo 2) martes .. ");
+				String nombre = vp.getMsj().tomarString("Nombre Asignatura");
+
+				vp.getView().ponerEnHorario(hinicial, diaSemana, nombre);
+				vp.getView().ponerEnHorario(hfinal, diaSemana, nombre);
+
+				vp.getView().getTabla().setModel(vp.getView().getModeloTabla());
+				vp.getMsj().mostrar("Agregar");
+			} catch (Exception e2) {
+				vp.getMsj().mostrar("Error");
+			}
+		}
+		if (comando.equals("Exit")) {
+			vp.getMsj().mostrar("Hasta Luego...");
+			System.exit(0);
+		}
 
 	}
 
--- src/main/java/view/Ventana.java
+++ src/main/java/view/Ventana.java
@@ -1,6 +1,10 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -11,29 +15,50 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class Ventana extends JFrame {
+
+	private JMenuItem exitMenuItem;
+	private JPanel horario;
+	private JButton btnAgregar;
+	private String[][] datos;
+	private JTable tabla;
+	private DefaultTableModel modeloTabla;
+	private String[] diasSemana = { "Hora", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+
 	public Ventana() {
 		setTitle("Horario");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1280, 720);
 		setLocationRelativeTo(null);
 		setLayout(null);
+		setResizable(false);
 
-		createMenuBar();
+		crearMenuBar();
 
-		JPanel horario = new JPanel();
+		btnAgregar = new JButton("Agregar Materia");
+		btnAgregar.setBounds(0, 0, 150, 40);
+		btnAgregar.setFocusable(false);
+		btnAgregar.setActionCommand("Agregar");
+		add(btnAgregar);
+
+		horario = new JPanel();
 		horario.setLayout(new BorderLayout());
 		horario.setBounds(420, 50, 800, 535);
 
-		// Initialize schedule data
-		String[] diasSemana = { "Hora", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
-		String[][] datos = new String[16][8];
-		datos[0][0] = "7 am";
+		datos = new String[16][8];
+
+		for (int i = 0; i < datos.length; i++) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(Calendar.HOUR_OF_DAY, 7 + i);
+			calendar.set(Calendar.MINUTE, 0);
+
+			datos[i][0] = new SimpleDateFormat("hh:mm a").format(calendar.getTime());
+		}
+
 		datos[0][1] = "Complejidad Algoritmica";
 		datos[1][1] = "Complejidad Algoritmica";
-		datos[15][0] = "10 pm";
 
-		DefaultTableModel modeloTabla = new DefaultTableModel(datos, diasSemana);
-		JTable tabla = new JTable(modeloTabla);
+		modeloTabla = new DefaultTableModel(datos, diasSemana);
+		tabla = new JTable(modeloTabla);
 		tabla.setRowHeight(32);
 
 		JScrollPane scrollPane = new JScrollPane(tabla);
@@ -43,17 +68,79 @@ public class Ventana extends JFrame {
 		add(horario);
 	}
 
-	private void createMenuBar() {
+	public void ponerEnHorario(int x, int y, String materia) {
+		datos[x][y] = materia;
+		modeloTabla = new DefaultTableModel(datos, diasSemana);
+
+	}
+
+	private void crearMenuBar() {
 		JMenuBar menuBar = new JMenuBar();
 
 		JMenu fileMenu = new JMenu("Menu");
-		JMenuItem exitMenuItem = new JMenuItem("Exit");
-		exitMenuItem.addActionListener(e -> System.exit(0));
+		exitMenuItem = new JMenuItem("Exit");
+		exitMenuItem.setActionCommand("Exit");
 		fileMenu.add(exitMenuItem);
 
 		menuBar.add(fileMenu);
 
 		setJMenuBar(menuBar);
+	}
+
+	public JTable getTabla() {
+		return tabla;
+	}
+
+	public void setTabla(JTable tabla) {
+		this.tabla = tabla;
+	}
+
+	public DefaultTableModel getModeloTabla() {
+		return modeloTabla;
+	}
+
+	public void setModeloTabla(DefaultTableModel modeloTabla) {
+		this.modeloTabla = modeloTabla;
+	}
+
+	public JMenuItem getExitMenuItem() {
+		return exitMenuItem;
+	}
+
+	public void setExitMenuItem(JMenuItem exitMenuItem) {
+		this.exitMenuItem = exitMenuItem;
+	}
+
+	public JPanel getHorario() {
+		return horario;
+	}
+
+	public void setHorario(JPanel horario) {
+		this.horario = horario;
+	}
+
+	public JButton getBtnAgregar() {
+		return btnAgregar;
+	}
+
+	public void setBtnAgregar(JButton btnAgregar) {
+		this.btnAgregar = btnAgregar;
+	}
+
+	public String[][] getDatos() {
+		return datos;
+	}
+
+	public void setDatos(String[][] datos) {
+		this.datos = datos;
+	}
+
+	public String[] getDiasSemana() {
+		return diasSemana;
+	}
+
+	public void setDiasSemana(String[] diasSemana) {
+		this.diasSemana = diasSemana;
 	}
 
 }
